48ce0678a1e3e521d3c07bfd4854a5e9851d7040
ElyvanBS <elyvanbs@gmail.com>
2025-05-18 18:18:47 -0300

    feat: Começa o css da caixa simples de membros

--- pages/conselho-de-mestres/js/script.js
+++ pages/conselho-de-mestres/js/script.js
@@ -24,3 +24,26 @@ const nav = document.querySelector(".nav");
             hamburger.style.right = '';
         }
     });
+
+    // Button Voltar ao topo
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('backToTop');
+
+    // Mostrar/esconder botão baseado na posição do scroll
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 300) { // Mostra depois de 300px de scroll
+            backToTopButton.classList.add('visible');
+        } else {
+            backToTopButton.classList.remove('visible');
+        }
+    });
+
+    // Função para rolar suavemente para o topo
+    backToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
+
